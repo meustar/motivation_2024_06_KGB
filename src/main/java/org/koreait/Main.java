20b08a3f4cbb1cd4c732510c34116aa457d97c49
meustar <meustar829@gmail.com>
2024-06-27 16:49:02 +0900

    ì계획ì1.. 구현해보자

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -2,6 +2,30 @@ package org.koreait;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // System.out.println("== motivation 실행 ==");
+
+        // 명령어) 등록
+        // motivation: (내용)
+        // source: (출처..)
+        // 1번 motivation이 등록 되었습니다.
+
+        // 명령어) 등록
+        // motivation: (내용)
+        // source: (출처..)
+        // 2번 motivation이 등록 되었습니다.
+
+        // 명령어) 목록
+        // System.out.println("== motivation 목록 ==");
+        // System.out.println("============================");
+        // 번호  / Source    /   motivation
+        // 1     /   !@@#    /   ~~~~~~
+        // 2     /   $#%^    /   !~!~~~
+
+        // 명령어) 삭제
+
+
+        // 명령어) 종료  -- scanner 사용할 예정.
+        // System.out.println("== motivation 종료 ==");
+        // 프로그램 종료
     }
 }
